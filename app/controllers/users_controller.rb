481a2e2800a735d59ee6b422300c5f4add48aa06
therobgeorge <georg1rd@gmail.com>
2021-12-27 14:35:36 -0500

    adds user create and authentication

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,18 @@
 class UsersController < ApplicationController
+  
+  def create
+    user = User.new(
+      name: params[:name],
+      email: params[:email],
+      password: params[:password],
+      password_confirmation: params[:password_confirmation],
+      phone_number: params[:phone_number]
+    )
+    if user.save
+      render json: { message: "User created!"}, status: :created
+    else
+      render json: { errors: user.erros.full_messages }, status: :bad_request
+    end
+  
+  end
 end
