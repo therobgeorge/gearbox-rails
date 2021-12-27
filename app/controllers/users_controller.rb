3e63efc21950aba6b87de51b1a9225098b4ac53b
therobgeorge <georg1rd@gmail.com>
2021-12-27 16:26:55 -0500

    adds needed CRUD actions to users controller with associated routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
   
+  before_action :authenticate_user, except: [:create]
+
   def create
     user = User.new(
       name: params[:name],
@@ -13,6 +15,46 @@ class UsersController < ApplicationController
     else
       render json: { errors: user.erros.full_messages }, status: :bad_request
     end
-  
   end
+
+  def show
+    user = User.find(params[:id])
+    if current_user.id == user.id
+      render json: user
+    else
+      render json: { message: "Not logged in with correct user" }
+    end
+  end
+
+  def update
+    user = User.find(params[:id])
+    if current_user.id == user.id
+      if params[:password] && params[:password_confirmation]
+        user.password = params[:password]
+        user.password_confirmation = params[:password_confirmation]
+      end
+      user.name = params[:name] || user.name
+      user.email = params[:email] || user.email
+      user.phone_number = params[:phone_number] || user.phone_number
+      if user.save
+        render json: user
+      else
+        render json: { errors: user.errors.full_messages, status: :unprocessable_entity }
+      end
+    else
+      render json: { message: "Not logged in" }
+    end
+  end
+
+  def destroy
+    user = User.find(params[:id])
+    if current_user.id == user.id
+      user.destroy
+      render json: { message: "User destroyed" }
+    else
+      render json: { message: "Not logged in"}
+    end
+    
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   
   #users
   post "/users" => "users#create"
+  get "/users/:id" => "users#show"
+  patch "/users/:id" => "users#update"
+  delete "users/:id" => "users#destroy"
 
   #sessions
   post "/sessions" => "sessions#create"
