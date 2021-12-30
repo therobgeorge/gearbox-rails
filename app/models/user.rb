caf06cffc3ef62e96e4456af9fd6b0d17ade2c1f
therobgeorge <georg1rd@gmail.com>
2021-12-30 13:49:51 -0500

    fixes associations for images to gear searlizer

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ApplicationRecord
   has_many :gears
-  has_many :images, through: :gears
 
   has_secure_password
   validates :email, presence: true, uniqueness: true
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -2,5 +2,4 @@ class UserSerializer < ActiveModel::Serializer
   attributes :id, :email, :phone_number
 
   has_many :gears
-  # has_many :images, through: :gears
 end
