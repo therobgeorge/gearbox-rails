73b0ba733cc6785c88966539b1be142e20484fde
therobgeorge <georg1rd@gmail.com>
2022-01-06 14:33:04 -0500

    adds cloudinary photo uploading

--- Gemfile
+++ Gemfile
@@ -39,3 +39,5 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem 'jwt'
 gem 'active_model_serializers'
+
+gem 'cloudinary'
--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -3,9 +3,12 @@ class ImagesController < ApplicationController
   before_action :authenticate_user
 
   def create
+    response = Cloudinary::Uploader.upload(params[:photo_url], resource_type: :auto)
+    cloudinary_url = response["secure_url"]
+
     image = Image.new(
       gear_id: params[:gear_id], 
-      photo_url: params[:photo_url]
+      photo_url: cloudinary_url
     )
     if image.save
       render json: image
